6fd343b496df8c164be5dceec3a1bb0ac1f9180f
satheesh <satheeshsaravanjs@gmail.com>
2025-03-25 23:23:10 +0530

    rxjs-of operator

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # rxjs
+# of operator 
+    - Create an observable from string or object or array 
+    - Example - viewers:Observables<string[]>= of(['car', 'bike'])
--- src/app/rxjs-practice/rxjs-practice.component.ts
+++ src/app/rxjs-practice/rxjs-practice.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, of, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-rxjs-practice',
@@ -10,33 +10,61 @@ import { Observable } from 'rxjs';
 export class RxjsPracticeComponent {
   agents!: Observable<string>;
   agentName: string = '';
+  private subscription!: Subscription;
+  studentnameList = ['Jhon','Doe','Smith', 'Angel'];
+  students$:Observable<string[]>= of(this.studentnameList);
+  studentName$:Observable<string> = of('Hanna');
+  studentObj = {
+    id:11,
+    Name:'Nirmal'
+  }
+  studentObservableObj$:Observable<any> = of(this.studentObj)
   constructor() { }
 
   ngOnInit() {
-    this.agents = new Observable(
-      function (observer) {
-        try {
-          observer.next('Ram');
-          // observer.next('Mark');
-          //   observer.next('Anto');
-          setTimeout(()=>{
-          observer.next('Mark');
-          },3000);
-          setTimeout(()=>{
-            observer.next('Anto');
-          },6000);
-        }
-        catch (e) {
-          observer.error(e)
-        }
-      }
-    );
+    //Basic observable creation 
+    // this.agents = new Observable(
+    //   function (observer) {
+    //     try {
+    //       observer.next('Ram');
+    //       // observer.next('Mark');
+    //       //   observer.next('Anto');
+    //       setTimeout(()=>{
+    //       observer.next('Mark');
+    //       },3000);
+    //       setTimeout(()=>{
+    //         observer.next('Anto');
+    //       },6000);
+    //     }
+    //     catch (e) {
+    //       observer.error(e)
+    //     }
+    //   }
+    // );
 
-    this.agents.subscribe(data => {
-      console.log(data);
-      this.agentName = data;
+    // this.agents.subscribe(data => {
+    //   console.log(data);
+    //   this.agentName = data;
+    // })
+//<--------------------------------------------------------->    
+    //Of operator Subcription
+    // Array to Observable 
+    this.students$.subscribe(data => {
+      console.log('of operator1', data)
     })
-    
+    //String to observable 
+    this.studentName$.subscribe(data => {
+      console.log('of operator2', data)
+    })
+    //Object to observable 
+    this.studentObservableObj$.subscribe(data => {
+      console.log('of operator3', data)
+    })
+  }
+  ngOnDestroy() {
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+    }
   }
   
 }
